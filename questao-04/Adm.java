f9a7793fe721b4928d4769f97b5e0e4eabbd27a0
joaoalvesxp <joaoalvesxp@alu.ufc.br>
2022-11-02 01:05:40 -0300

    Refazendo Questão 4.

---
+++ questao-04/Adm.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Adm {
+    ArrayList<Candidato> candidatos = new ArrayList<>();
+    ArrayList<Usuario>  eleitores = new ArrayList<>();
+    int numeorDaUrna = 1;
+
+    Urna urna = new Urna(candidatos, eleitores, numeorDaUrna);
+
+    Servidor
+}
---
+++ questao-04/Servidor.java
@@ -0,0 +1,8 @@
+public class Servidor {
+
+
+
+    public static void receberUrna () {
+
+    }
+}
